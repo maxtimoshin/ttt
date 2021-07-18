c45408796167b37ccb8a86469ece40a1ea37fae5
Maxim94 <maxtimoshin94@gmail.com>
2021-07-18 16:42:22 +0300

    result modal window added

--- src/components/result-modal/result-modal.component.jsx
+++ src/components/result-modal/result-modal.component.jsx
@@ -2,17 +2,12 @@ import React from 'react'
 import './result-modal.styles.scss'
 
 const ResultModal = ({ resultModal, result, closeResultWindow }) => {
-
-    const restartGame = () => {
-        closeResultWindow()
-    }
-
     return (
         <div className={resultModal ? 'result-modal' : 'result-modal-hidden'}>
             <div className='modal-bg'></div>
             <div className='modal'>
                 <span>{result.winner == 'X' || result.winner == 'O' ? `ПОБЕДИЛ ИГРОК ${result.winner}` : 'НИЧЬЯ'}</span>
-                <button className='modal-btn' onClick={restartGame}>PLAY AGAIN</button>
+                <button className='modal-btn' onClick={closeResultWindow}>PLAY AGAIN</button>
             </div>
         </div>
     )
